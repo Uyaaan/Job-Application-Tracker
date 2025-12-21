b7e3eed92389f54464ad61c2c0f0517b5982aa30
Uyaaan <uyanqwerty@gmail.com>
2025-12-22 01:34:05 +0800

    Registration is now functioning

--- src/models/User.js
+++ src/models/User.js
@@ -1,23 +1,26 @@
 import mongoose from "mongoose";
 
-const UserSchema = new mongoose.Schema({
-  clerkId: {
-    type: String,
-    required: true,
-    unique: true,
+const UserSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    // We removed clerkId. These are optional standard fields:
+    image: String,
+    emailVerified: Date,
   },
-  email: {
-    type: String,
-    required: true,
-  },
-  spreadsheetId: {
-    type: String,
-    default: null, // Will be null until we auto-create their sheet
-  },
-  createdAt: {
-    type: Date,
-    default: Date.now,
-  },
-});
+  { timestamps: true }
+);
 
+// Check if the model exists before compiling it (prevents hot-reload errors)
 export default mongoose.models.User || mongoose.model("User", UserSchema);
